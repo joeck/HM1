240301272aa4f796f1248288da469da8fe20101f
plambjoe <plambjoe@students.zhaw.ch>
2020-10-06 12:43:25 +0200

    improves plot

--- P02/IT19a_ZH6_S2_Aufg3.py
+++ P02/IT19a_ZH6_S2_Aufg3.py
@@ -1,22 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, ax = plt.subplots()
-x = np.arange(6, 1000, 6)
-def ss(n):
-    if (n <= 6):
-        return 1
-    return np.sqrt(2 - (2 * np.sqrt(1 - (pow(s(n/2),2)/4))))
-
-#y = [i*s(i) for i in x]
+x = []
 y = []
 n = 6
 s = 1
-for i in range(30):
+for i in range(20):
     s = np.sqrt(2 - 2 * np.sqrt(1 - (s ** 2 / 4)))
     n = n * 2
     y.append(s / 2 * n)
+    x.append(n)
 
-print(y)
-ax.plot(y, label="S(n)")
+plt.plot(x, y)
+plt.xlim(1,1000)
+plt.ylim(3.1,3.15)
+plt.xlabel("Anzahl Ecken des Vielecks")
+plt.ylabel("Annäherung an Pi")
+plt.grid()
 
+# Grosse n werden wegen falsch gerundet (float)
