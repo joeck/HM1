aa4b0ec2624aff684ebf5438d179cad80fcc2ca3
plambjoe <plambjoe@students.zhaw.ch>
2020-11-02 00:11:24 +0100

    finally done

--- P06/IT19a_ZH6_S6_Aufg2.py
+++ P06/IT19a_ZH6_S6_Aufg2.py
@@ -6,9 +6,10 @@ def IT19a_ZH6_S6_Aufg2(A,b):
     aODM = oDM[:,:-1]
     det = determinante(aODM)
 
-    print("Solution: " + str(x))
+    print("\n-------------")
     print("Dreiecksmatrix: \n" + str(aODM))
     print("Determinante: " + str(det))
+    print("Solution: " + str(x))
 
 def obereDreiecksMatrix(A, b):
     rows = A[0]
@@ -53,3 +54,31 @@ def determinante(A):
 test = np.array([[20,10,0],[50,30,20],[200,150,100]])
 b = np.array([[150], [470], [2150]])
 IT19a_ZH6_S6_Aufg2(test, b)
+
+A1 = np.array([[4,-1,-5,-5],[-12,4,17,19],[32,-10,-41,-39]])
+a1 = np.delete(A1,3,axis = 1)
+b1 = A1[:,3]
+IT19a_ZH6_S6_Aufg2(a1, np.vstack(b1))
+solved = np.linalg.solve(a1,b1)
+print("Numpy sais: ",solved)
+
+A2 = np.array([[2,7,3,25],[-4,-10,0,-24],[12,34,9,107]])
+a2 = np.delete(A2,3,axis = 1)
+b2 = A2[:,3]
+IT19a_ZH6_S6_Aufg2(a2, np.vstack(b2))
+solved = np.linalg.solve(a2,b2)
+print("Numpy sais: ",solved)
+
+A3 = np.array([[-2,5,4,1],[-14,38,22,40],[6,-9,-27,75]])
+a3 = np.delete(A3,3,axis = 1)
+b3 = A3[:,3]
+IT19a_ZH6_S6_Aufg2(a3,np.vstack(b3))
+solved = np.linalg.solve(a3,b3)
+print("Numpy sais: ",solved)
+
+A4 = np.array([[-1,2,3,2,5,4,3,-1,-11],[3,4,2,1,0,2,3,8,103],[2,7,5,-1,2,1,3,5,53],[3,1,2,6,-3,7,2,-2,-20],[5,2,0,8,7,6,1,3,95],[-1,3,2,3,5,3,1,4,78],[8,7,3,6,4,9,7,9,131],[-3,14,-2,1,0,-2,10,5,-26]])
+a4 = np.delete(A4,8,axis = 1)
+b4 = A4[:,7]
+IT19a_ZH6_S6_Aufg2(a4,np.vstack(b4))
+solved = np.linalg.solve(a4,b4)
+print("Numpy sais: ",solved)
